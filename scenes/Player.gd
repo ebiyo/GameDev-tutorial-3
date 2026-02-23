2d43bfafd760c6f8d9525f4432ca143d587d82c9
ebiyo <ariefridzki@gmail.com>
2026-02-23 13:18:15 +0700

    Replaced sprite ship with character, added double jump

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
-@export var gravity = 200.0
-@export var walk_speed = 200
-@export var jump_speed = -300
+@export var gravity = 800.0
+@export var walk_speed = 300
+@export var jump_speed = -400
+@export var jump_times = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,8 +12,12 @@ func _ready() -> void:
 func _physics_process(delta):
 	velocity.y += delta * gravity
 	
-	if is_on_floor() and Input.is_action_just_pressed('ui_up'):
+	if is_on_floor():
+		jump_times = 2
+	
+	if Input.is_action_just_pressed('ui_up') && jump_times > 0:
 		velocity.y = jump_speed
+		jump_times -= 1
 		
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -walk_speed
